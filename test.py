1ec8dcf1d6de42167336ba0efa3b57b0fc528f87
Jack Trowbridge <jytrowbridge@gmail.com>
2020-10-01 13:27:08 -0400

    Finish initial get_path logic

--- test.py
+++ test.py
@@ -36,11 +36,7 @@ def get_related_artists(artist_name='', artist_id=False):
         url = f'https://api.spotify.com/v1/artists/{artist_id}/related-artists'
 
         r_raw = get_request(url, headers=headers)
-
-        # print(json.dumps(r_raw.json(), indent=2))
-
         artists = []
-
         for artist in r_raw.json()['artists']:
             image_url = ''
             max_size = 0
@@ -48,10 +44,13 @@ def get_related_artists(artist_name='', artist_id=False):
                 if int(image['height']) > max_size:
                     image_url = image['url']
                     max_size = int(image['height'])
+
             artists.append({
                 'name': artist['name'],
                 'id': artist['id'],
-                'image_url': image_url
+                'image_url': image_url,
+                'parent_id': artist_id,
+                'parent_name': artist_name
             })
 
         return artists
@@ -67,34 +66,49 @@ def find_path_bw_artists(artist1, artist2):
             find related artists
             if any artists are target, return
             else, add children to artists
+
+        the calls to api are taking forever...
+        efficiency idea:
+            instead of calling 'get_related_artists' every time, just add a placeholder
+            when get to placeholder, then call get_related_artists
     '''
     artists = get_related_artists(artist1)
     seen = {}
     parents = {}
     while len(artists) > 0:
         artist = artists.pop(0)
+
+        if artist['id'] in seen:
+            continue
+        seen[artist['id']] = True
+        parents[artist['id']] = {
+                                    'id': artist['parent_id'],
+                                    'name': artist['parent_name']
+                                }
+
         if artist['name'].upper() == artist2.upper():
             # print(parents)
             # return 'path found!!!'
             print('path found!!!')
             path = [artist['name']]
             artist_id = artist['id']
-            while artist_id in parents:
+            artist_name = artist['name']
+            while artist_name.upper() != artist1.upper():
+                print(artist_name)
                 parent = parents[artist_id]
-                path += parent['name']
+                path += [parent['name']]
                 artist_id = parent['id']
-            return path
+                artist_name = parent['name']
+                return path
 
-        if artist['id'] in seen:
-            continue
-        seen[artist['id']] = True
-
-        related_artists = get_related_artists(artist_id=artist['id'])
+        related_artists = get_related_artists(
+                                artist_name=artist['name'],
+                                artist_id=artist['id']
+                            )
         artists += related_artists
-        for related_artist in related_artists:
-            parents[related_artist['id']] = {'id': artist['id'], 'name': artist['name']}
+
 
 if __name__ == "__main__":
     # print(get_artist_id('obituary'))
     # print(get_related_artists('obituary'))
-    print(find_path_bw_artists('obituary', 'death'))
+    print(find_path_bw_artists('obituary', 'metallica'))
